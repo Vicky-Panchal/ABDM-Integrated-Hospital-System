d54b34fcfa69d52390b4802b5b2cffb85cdd67a1
Vicky Panchal <vmp9905@gmail.com>
2024-03-21 21:25:22 +0530

    march 21

--- backend/src/main/java/com/hadproject/dhanvantari/config/SecurityConfiguration.java
+++ backend/src/main/java/com/hadproject/dhanvantari/config/SecurityConfiguration.java
@@ -40,7 +40,6 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .cors().and()
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(req ->
                         req.requestMatchers(WHITE_LIST_URL)

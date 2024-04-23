acd31794f8dd6af79fd7688adf0870a080f237e1
Adarsh Tripathi <adarshphysics1973@gmail.com>
2024-04-23 23:30:23 +0530

    validation in ChangePasswordPage

--- frontend/src/components/ChangePasswordPage.js
+++ frontend/src/components/ChangePasswordPage.js
@@ -7,16 +7,41 @@ const ChangePasswordPage = () => {
   const [currentPassword, setcurrentPassword] = useState("");
   const [newPassword, setNewPassword] = useState("");
   const [confirmationPassword, setConfirmPassword] = useState("");
+  const [newPasswordError, setNewPasswordError] = useState("");
+  const [confirmPasswordError, setConfirmPasswordError] = useState("");
   const userId = JSON.parse(localStorage.getItem("loggedInUser")).user_id;
   const token = JSON.parse(localStorage.getItem("loggedInUser")).access_token;
   const navigate = useNavigate();
 
+  const validatePassword = (password) => {
+    const regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*])(?=.{8,})/;
+    return regex.test(password);
+  };
+
   const handleChangePassword = async () => {
+    // Validate new password
+    if (!validatePassword(newPassword)) {
+        setNewPasswordError(
+          "Password must be more than 8 characters and contain both numbers and letters/special characters"
+        );
+        return;
+      } else {
+        setNewPasswordError("");
+      }
+  
+      // Validate confirm password
+      if (newPassword !== confirmationPassword) {
+        setConfirmPasswordError("Passwords do not match.");
+        return;
+      } else {
+        setConfirmPasswordError("");
+      }
+
     try {
       const response = await axios.patch(
         "http://localhost:8081/api/v1/users/changePassword",
         {
-        //   userId: userId,
+          userId: userId,
           currentPassword: currentPassword,
           newPassword: newPassword,
           confirmationPassword: confirmationPassword,
@@ -65,6 +90,7 @@ const ChangePasswordPage = () => {
             value={newPassword}
             onChange={(e) => setNewPassword(e.target.value)}
           />
+          {newPasswordError && <div className="error">{newPasswordError}</div>}
         </div>
         <div className="form-group">
           <label htmlFor="confirmPassword">Confirm Password:</label>
@@ -74,6 +100,11 @@ const ChangePasswordPage = () => {
             value={confirmationPassword}
             onChange={(e) => setConfirmPassword(e.target.value)}
           />
+
+          {confirmPasswordError && (
+            <div className="error">{confirmPasswordError}</div>
+          )}
+
         </div>
         <button onClick={handleChangePassword}>Submit</button>
       </div>

e87d3443cc46d698834e67903731da74ba7745c8
Parag <parag0501ds@gmail.com>
2024-05-02 11:11:44 +0530

    Created AdminDashboard and implemented it's multiple functionalities such as ViewAllPatients, ViewAllDoctors, and AppointmentsBooked. Also created their css

--- frontend/src/components/AdminDashboard/AdminDashboard.js
+++ frontend/src/components/AdminDashboard/AdminDashboard.js
@@ -1,12 +1,76 @@
 // AdminDashboard.js
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import AppointmentsBooked from "./AppointmentsBooked";
+import ViewAllPatients from "./ViewAllPatients";
+import ViewAllDoctors from "./ViewAllDoctors";
+import Navbar from "../navbar";
+import "../../Styles/AdminDashboard/adminDashboard.css";
 
 const AdminDashboard = () => {
+  const [selectedButton, setSelectedButton] = useState(null);
+
+  useEffect(() => {
+    setSelectedButton("appointments");
+  }, []);
+
+  const handleButtonClick = (buttonId) => {
+    setSelectedButton(buttonId);
+  };
+
   return (
     <div>
-      <h2>Admin Dashboard</h2>
-      {/* Add your admin dashboard content here */}
+      <div>
+        <Navbar />
+      </div>
+      <div className="admin-dashboard-container">
+        <div className="functionalities">
+          <div>
+            <button
+              className={selectedButton === "patients" ? "active" : ""}
+              onClick={() => handleButtonClick("patients")}
+            >
+              View All Patients
+            </button>
+          </div>
+          <div>
+            <button
+              className={selectedButton === "doctors" ? "active" : ""}
+              onClick={() => handleButtonClick("doctors")}
+            >
+              View All Doctors
+            </button>
+          </div>
+          <div>
+            <button
+              className={selectedButton === "appointments" ? "active" : ""}
+              onClick={() => handleButtonClick("appointments")}
+            >
+              Appointments Booked
+            </button>
+          </div>
+          <div>
+            <button
+              className={selectedButton === "documents" ? "active" : ""}
+              onClick={() => handleButtonClick("documents")}
+            >
+              Document Sharing History
+            </button>
+          </div>
+          <div>
+            <button
+              className={selectedButton === "logs" ? "active" : ""}
+              onClick={() => handleButtonClick("logs")}
+            >
+              Logs
+            </button>
+          </div>
+        </div>
+        <div className="content">
+          {selectedButton === "patients" && <ViewAllPatients />}
+          {selectedButton === "doctors" && <ViewAllDoctors />}
+          {selectedButton === "appointments" && <AppointmentsBooked />}
+        </div>
+      </div>
     </div>
   );
 };

b2ce26d49cb548bd1b2f0fc9300225b94087fede
AdarshTripathi-iiitb <adarshphysics1973@gmail.com>
2024-05-02 18:01:38 +0530

    notificationapiadd

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -1,73 +1,145 @@
 // Navbar.js
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "../Styles/navbar.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBell , faUser } from "@fortawesome/free-solid-svg-icons";
+import axios from "axios";
 
 const Navbar = () => {
   const navigate = useNavigate();
 
   const [showNotifications, setShowNotifications] = useState(false);
+  const [notifications, setNotifications] = useState([]);
 
-  const handleNotificationClick = () => {
+  useEffect(() => {
+    // Fetch notifications from the server
+    const fetchNotifications = async () => {
+      const accessToken = localStorage.getItem("token")
+      try {
+        const response = await axios.get("http://localhost:8081/api/v1/notifications", {
+          headers: {
+            Authorization: `Bearer ${accessToken}`
+          }
+        });
+        setNotifications(response.data);
+      } catch (error) {
+        console.error("Error fetching notifications:", error);
+      }
+    };
+    fetchNotifications();
+  }, []);
+
+
+  const handleNotificationClick = async () => {
+    if (!showNotifications) {
+      // Fetch notifications from the server
+      const accessToken = localStorage.getItem("token");
+      try {
+        const response = await axios.get("http://localhost:8081/api/v1/notifications", {
+          headers: {
+            Authorization: `Bearer ${accessToken}`
+          }
+        });
+        setNotifications(response.data);
+      } catch (error) {
+        console.error("Error fetching notifications:", error);
+      }
+    } else {
+      // Mark all notifications as read
+      await markAllNotificationsAsRead();
+      setNotifications(null);
+    }
+
+    // Toggle the showNotifications state
     setShowNotifications(!showNotifications);
   };
+
+  const markAllNotificationsAsRead = async () => {
+    try {
+      const accessToken = localStorage.getItem("token");
+      await axios.post("http://localhost:8081/api/v1/notifications/markAllAsRead", null, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`
+        }
+      });
+      console.log("All notifications marked as read.");
+    } catch (error) {
+      console.error("Error marking notifications as read:", error);
+    }
+  };
+
+
 
   const handleProfileClick = () => {
     navigate("/ProfilePage");
   };
 
-  const notifications = [
-    {
-      id: 1,
-      title: "Consent Request",
-      message: "Doctor Adarsh requested for the document consent.",
-      date: "2024-04-28T22:20:00",
-      url: "google.com",
-      isRead: "unread"
-    },
-    {
-      id: 2,
-      title: "Cancel Appointment",
-      message: "Your tomorrow's appointment has been canceled with doctor Keshav.",
-      date: "2024-04-15T18:45:00",
-      url: "",
-      isRead: "read"
-    },
-    {
-      id: 3,
-      title: "Document Sent",
-      message: "Your health documents have been shared to Doctor Keshav.",
-      date: "2024-03-01T12:00:00",
-      url: "google.com",
-      isRead: "unread"
-    },
-    {
-      id: 4,
-      title: "Consent Request",
-      message: "Doctor Adarsh requested for the document consent.",
-      date: "2024-01-28T09:30:00",
-      url: "google.com",
-      isRead: "unread"
-    },
-    {
-      id: 5,
-      title: "Cancel Appointment",
-      message: "Your tomorrow's appointment has been canceled with doctor Keshav.",
-      date: "2023-12-15T18:45:00",
-      url: "",
-      isRead: "read"
-    },
-    {
-      id: 6,
-      title: "Document Sent",
-      message: "Your health documents have been shared to Doctor Keshav.",
-      date: "2022-07-01T12:00:00",
-      url: "google.com",
-      isRead: "unread"
-    }
-  ];
+  // const notifications = [
+  //   {
+  //     id: 1,
+  //     title: "Consent Request",
+  //     message: "Doctor Adarsh requested for the document consent.",
+  //     date: "2024-04-28T22:20:00",
+  //     url: "google.com",
+  //     isRead: "unread"
+  //   },
+  //   {
+  //     id: 2,
+  //     title: "Cancel Appointment",
+  //     message: "Your tomorrow's appointment has been canceled with doctor Keshav.",
+  //     date: "2024-04-15T18:45:00",
+  //     url: "",
+  //     isRead: "read"
+  //   },
+  //   {
+  //     id: 3,
+  //     title: "Document Sent",
+  //     message: "Your health documents have been shared to Doctor Keshav.",
+  //     date: "2024-03-01T12:00:00",
+  //     url: "google.com",
+  //     isRead: "unread"
+  //   },
+  //   {
+  //     id: 4,
+  //     title: "Consent Request",
+  //     message: "Doctor Adarsh requested for the document consent.",
+  //     date: "2024-01-28T09:30:00",
+  //     url: "google.com",
+  //     isRead: "unread"
+  //   },
+  //   {
+  //     id: 5,
+  //     title: "Cancel Appointment",
+  //     message: "Your tomorrow's appointment has been canceled with doctor Keshav.",
+  //     date: "2023-12-15T18:45:00",
+  //     url: "",
+  //     isRead: "read"
+  //   },
+  //   {
+  //     id: 6,
+  //     title: "Document Sent",
+  //     message: "Your health documents have been shared to Doctor Keshav.",
+  //     date: "2022-07-01T12:00:00",
+  //     url: "google.com",
+  //     isRead: "unread"
+  //   }
+  // ];
+
+//   const markAllNotificationsAsRead = async () => {
+//     try {
+//         const accessToken = localStorage.getItem("token");
+//         await axios.post("http://localhost:8081/api/v1/notifications/markAllAsRead", null, {
+//             headers: {
+//                 Authorization: `Bearer ${accessToken}`
+//             }
+//         });
+//         // setNotifications(null);
+//         console.log("All notifications marked as read.");
+//     } catch (error) {
+//         console.error("Error marking notifications as read:", error);
+//     }
+// };
 
   const getTimeAgo = (creationTime) => {
     const currentTime = new Date();
@@ -143,22 +215,23 @@ const Navbar = () => {
 
               <hr/ >
               
-              {notifications.map((notification) => (
-                <div
-                  key={notification.id}
-                  className={`notification ${
-                    notification.isRead ? "read" : "unread"
-                  }`}
-                >
-                  <div className="notification-title-message">
-                    <p><strong>{notification.title} : </strong>{notification.message}</p>
+              {notifications === null ? (
+                <p>No more notifications</p>
+              ) : (
+                notifications.map((notification) => (
+                  <div
+                    key={notification.id}
+                    className={`notification ${notification.isRead ? "read" : "unread"}`}
+                  >
+                    <div className="notification-title-message">
+                      <p><strong>{notification.title} : </strong>{notification.message}</p>
+                    </div>
+                    <div className="notification-time">
+                      <p>{getTimeAgo(notification.createdAt)}</p>
+                    </div>
                   </div>
-                  <div className="notification-time">
-                    <p>{getTimeAgo(notification.date)}</p>
-                  </div>
-                  
-                </div>
-              ))}
+                ))
+              )}
             </div>
 
           )}
